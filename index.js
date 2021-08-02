7b722a251b3fc0294bfbeac6e3d0fccf91e7cdab
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-02 00:39:47 -0600

    [ADD] Middleware Factura, Categoria

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const usuariosRouter = require('./routes/usuarios-router');
+const categoriasRouter = require('./routes/categorias-router');
+const facturasRouter = require('./routes/facturas-router');
+
 const db = require('./modules/database');
 
 const app = express();
@@ -10,6 +13,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/usuarios', usuariosRouter);
+app.use('/categorias', categoriasRouter);
+app.use('/facturas', facturasRouter);
+
 
 app.get('/', (req, res)=> {
     res.send('Backend REST-API Nany');
