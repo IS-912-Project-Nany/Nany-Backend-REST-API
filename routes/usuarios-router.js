4239a93b6d58431f6feefc0f06127e3afc7dec78
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-08-03 18:05:51 -0600

    [FIX] CRUD motoristas

--- routes/usuarios-router.js
+++ routes/usuarios-router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const usuario = require("../models/usuario");
 const router = express.Router();
 
@@ -37,4 +38,71 @@ router.post("/", (req, res) => {
     });
 });
 
+//Obtener todos los motoristas
+router.get('/motoristas', (req, res)=>{
+    usuario.find({
+        "tipoUsuario.idUsuario": 2  //El codigo motorista
+    })
+    .then(result=>{
+        res.send(result);
+        res.end();
+    }).catch(error=>{
+        res.send(error);
+        res.end();
+    });
+});
+
+//Obtener motorista
+router.get('/motoristas/:idMotorista', (req, res)=>{
+    usuario.find({
+        _id: mongoose.Types.ObjectId(req.params.idMotorista),
+        "tipoUsuario.idUsuario": 2 
+    })
+    .then(result=>{
+        res.send(result[0]);
+        res.end();
+    }).catch(error=>{
+        res.send(error);
+        res.end();
+    });
+});
+
+//Actualizar motorista
+router.put('/motoristas/:idMotorista', (req, res)=>{
+    usuario.updateOne(
+        {
+            _id: mongoose.Types.ObjectId(req.params.idMotorista),
+            "tipoUsuario.idUsuario": 2 
+        },
+        {
+            $set: {
+                "tipoUsuario.motoristaInfo.0.estadoAdmision": req.body.estadoAdmision
+            }
+        }
+    )
+    .then(result=>{
+        res.send(result);
+        res.end();
+    }).catch(error=>{
+        res.send(error);
+        res.end();
+    });
+});
+
+//Eliminar motorista 
+router.delete('/motoristas/:idMotorista', (req, res)=>{
+    usuario.deleteOne(
+        {
+            _id: mongoose.Types.ObjectId(req.params.idMotorista),
+            "tipoUsuario.idUsuario": 2 
+        }
+    )
+    .then(result=>{
+        res.send(result);
+        res.end();
+    }).catch(error=>{
+        res.send(error);
+        res.end();
+    });
+})
 module.exports = router;
