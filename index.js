5824f3522421824cd2ab341714ffdeb2cec60fbe
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-10 00:32:02 -0600

    [ADD] Env Vars

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+dotenv.config();
 
 const usuariosRouter = require('./routes/usuarios-router');
 const categoriasRouter = require('./routes/categorias-router');
 const ordenesRouter = require('./routes/ordenes-router');
 
 const db = require('./modules/database');
-
+const port = process.env.PORT || 8888;
 const app = express();
 
 app.use(cors());
@@ -23,6 +25,6 @@ app.get('/', (req, res)=> {
     res.end();
 });
 
-app.listen(process.env.PORT || '8888', ()=>{
+app.listen(port, ()=>{
     console.log('Servidor Online');
 });
