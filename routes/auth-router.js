89f2864be58e0ec6592d2a13cc7a877a4d2dd517
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-11 21:56:12 -0600

    [ADD] Auth Usuario

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../modules/database');
 const bcrypt = require('bcrypt');
+const usuario = require('../models/usuario');
 
 
 router.post('/', (req, res) => {
@@ -9,6 +10,39 @@ router.post('/', (req, res) => {
         correo: req.body.correo,
         contraseña: req.body.contraseña,
     }
+
+    usuario.find(
+        {
+            correo: req.body.correo
+        })
+        .then((result) => {
+            console.log(result);
+            if(result != ''){
+                 bcrypt.compare(req.body.contraseña, result[0].contraseña, (error, coinciden) => {
+                     if (error)
+                         console.log("Error Comprobando...", error)
+                     else {
+                        if(coinciden) {
+                            let resultAuth = result[0];
+                            resultAuth.contraseña = '';
+                            res.send({ code: 1, message: "Usuario Autenticado con Éxito", usuario: resultAuth});
+                            res.end();
+                        } else {
+                            res.send({ code: 0, message: "Contraseña Incorrecta" });
+                            res.end();
+                        }
+                    }
+                });
+            } else {
+                res.send({code: 0, menssage: "Correo No Encontrado"});
+                res.end();
+            };
+        })
+        .catch((error) => {
+            res.send(error);
+            res.end();
+        });
+
 });
 
 module.exports = router;
