9def1d66016d2b827c7ab9d67f91fb5a3f4227cb
franciaavila28 <47584146+franciaavila28@users.noreply.github.com>
2021-08-12 01:49:54 -0600

    [FIX] TipoUsuario type

--- models/tipoUsuario.js
+++ models/tipoUsuario.js
@@ -5,7 +5,7 @@ const schema = new mongoose.Schema({
     _id: false,
     idUsuario: Number,
     tipo: String,
-    motoristaInfo: {type: [motorista], required: false} 
+    motoristaInfo: {type: {motorista}, required: false} 
 });
 
 module.exports = schema;
